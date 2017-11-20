eb1cff8bfa80f62047b50449837ad278b813181d
Eduardo Schoepf <eduardo.schoepf@hotmail.com>
2017-11-20 01:08:50 +0100

    [UPDATE] - Create a "check off" shopping List application.

---
+++ app.js
@@ -0,0 +1,54 @@
+"use strict";
+
+(function () {
+        angular.module("ShoppingListCheckOff", [])
+            .factory("ShoppingListCheckOffService", [ShoppingListCheckOffService])
+            .controller("ToBuyController", ["ShoppingListCheckOffService", ToBuyController])
+            .controller("AlreadyBoughtController", ["ShoppingListCheckOffService", AlreadyBoughtController]);
+
+        function ToBuyController(ShoppingListCheckOffService) {
+            var toBuy = this;
+            toBuy.items = ShoppingListCheckOffService.getToBuyList();
+        }
+
+        function AlreadyBoughtController(ShoppingListCheckOffService) {
+            var alreadyBought = this;
+            alreadyBought.items = ShoppingListCheckOffService.getAlreadyBought();
+        }
+
+        function ShoppingListCheckOffService() {
+            var toBuyList = [
+                { name: "cookies", quantity: 10, boughtItem: boughtItem },
+                { name: "yogurt", quantity: 5, boughtItem: boughtItem },
+                { name: "cheese", quantity: 3, boughtItem: boughtItem },
+                { name: "eggs", quantity: 24, boughtItem: boughtItem },
+                { name: "milk", quantity: 6, boughtItem: boughtItem },
+                { name: "bacon", quantity: 1, boughtItem: boughtItem },
+                { name: "orange", quantity: 5, boughtItem: boughtItem },
+                { name: "apple", quantity: 5, boughtItem: boughtItem },
+                { name: "pears", quantity: 5, boughtItem: boughtItem }
+            ];
+            var alreadyBought = [];
+
+            function getToBuyList() {
+                return toBuyList;
+            }
+
+            function getAlreadyBought() {
+                return alreadyBought;
+            }
+
+            function boughtItem(itemId) {
+                alreadyBought.push(toBuyList[itemId]);
+                toBuyList.splice(itemId, 1);
+            }
+
+            return {
+                getToBuyList: getToBuyList,
+                getAlreadyBought: getAlreadyBought,
+                boughtItem: boughtItem
+            }
+        }
+
+    }
+)();
